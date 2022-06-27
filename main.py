c6f4b49c4f49e2735f3fbd016a507522fc89e4ed
MrCheat <40684650+MrCheatEugene@users.noreply.github.com>
2022-06-27 10:33:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ speaker = ev3.speaker
 # Write your program here.
 #ev3.speaker.beep()
 titles=[]
+task = 0
 files = []
 import os
 for file in sorted(os.listdir("./")):
@@ -52,6 +53,8 @@ def renderPlaying(file):# Рендерим играющую песню
     screen.draw_image(int((maxW-10)/4),int(3), "./vinyl.png")# Рисуем картинку посередине экрана
     screen.draw_text(10,105, str(file), text_color=Color.BLACK, background_color=None) # Пишем название композиции(файла)
 def play(file,titles,boxList,page):# Проиграть что-то
+    print(file)
+    print(page)
     # А вот дальше я не очень понимаю как оно работает,поэтому комментарии будут максимально поверхностными, сорян
     speaker_lock.acquire(False) #???
     def play_in_background_thread():# Проиграть композицию в отдельном потоке
@@ -62,7 +65,8 @@ def play(file,titles,boxList,page):# Проиграть что-то
         finally:#Когда композиция закончилась
             speaker_lock.release()# Освобождаем динамик
             isLocked = False
-            renderMenu(titles,page,boxList) # Рендерим меню
+            print(page)
+            renderMenu(titles,page-1,boxList) # Рендерим меню
     thread = _thread.start_new_thread(play_in_background_thread, ()) # Создаём поток с функцией "проиграть в отдельном потоке"
     try:# Я хз что тут происходит, не шарю за генераторы :/
         while speaker_lock.locked():
@@ -79,13 +83,13 @@ def renderMenu(titles,page,boxList):
     currY=0# Текущая позиция рендера по Y
     boxSize=20# Размер контейнера в меню
     font = Font(size=15)# Создаём шрифт
-    offset=int(page*int(maxH/boxSize)-1) # Учитываем страницу
+    offset=int(page*int(maxH/boxSize)-1) # Вычисляем параметр отсчёта
     if(offset > len(titles)):# Если произошла ошибка в расчётах
         offset = int(len(titles)/2) # Пытаемся её исправить
     if(offset<0):# Если число отрицательное
         offset = 0# Возвращаемся к 0
-    #print("Offset "+str(offset)) # Отладочная информация
-    #print("Page "+str(page))# Отладочная информация
+    print("Offset "+str(offset)) # Отладочная информация
+    print("Page "+str(page))# Отладочная информация
     ev3.screen.set_font(font)# Ставим шрифт
     while (currY < maxH):# Пока не закончили рендерить:
         I=int(currY/boxSize)+offset # I- Внутренний параметр, индекс названия файла 
@@ -103,21 +107,63 @@ selected =0# Выбранный элемент
 prevselected =0# Старый выбранный элемент(для очистки)
 selectedINDEX=0# Index выбранного элемента
 renderMenu(titles,0 ,boxList) # Рендерим меню
+def playMusic(selectedINDEX,files,page,filesSelectIndex):
+        selectedINDEX=selectedINDEX-1 # Я чёрт возьми не помню зачем это нужно
+        #page =0
+        #if(page >1 and selectedINDEX >=1): # Если и страница и выбранный index больше 1, то
+        #    filesSelectIndex =int((((page*4))+selectedINDEX)-3)# Индекс файла вычисляется так
+        #elif(page >1 and selectedINDEX <1):# Если страница больше 1, и выбранный index меньше 1 то 
+        #    filesSelectIndex =int((page*6)-1)# Вычисляем так
+       # elif(page ==1):# Если страница = 1(зачем именно так я не знаю, но оно работает)
+       #     filesSelectIndex=int(page*6-selectedINDEX-1)# Вычисляем вот так
+       # else: # Если это всё не наш случай, то
+        #    filesSelectIndex=int(selectedINDEX) # Сдаёмся и ставим индекс файла на выбранный вами элемент(если страница - 0, всё будет нормально, если нет - всё не будет так хорошо)
+        # DEBUG INFO / ОТЛАДОЧНАЯ ИНФОРМАЦИЯ
+        #print(page >0)
+        #print(page)
+        #print(filesSelectIndex)
+        #print(files[filesSelectIndex])
+        boxList= []# Список "коробок" в меню(содержит его высоту)
+        wait_first([play(files[filesSelectIndex],titles,boxList,page)])# Создаём генератор?
+        tasks = [play(files[filesSelectIndex],titles,boxList,page)] # Создаём задачу
+        task = tasks[0]
+#        print(files[filesSelected])
+        for t in tasks: # Исполняем задачи
+            next(t)# Исполняем каждую задачу
+        blocking_wait(10)# Даём системе подумать
+        time.sleep(0.250) # Задержка для кнопок
+        return task
+filesSelectIndex =0
 while (True):# Цикл
     #if(page == 0):
     #    page = 1
     buttonsList=ev3.buttons.pressed()# Список нажатых кнопок
-    if(Button.DOWN in buttonsList and page+1 <= int((len(titles)/int(120/20))+1) and page+1 >1): # Если нажата кнопка "вниз", и мы можем переключится на следующую страницу - переключаемся
+    if(Button.DOWN in buttonsList and page+1 <= int((len(titles)/int(120/20))+1) and page+1 >0 and speaker_lock.locked()== False): # Если нажата кнопка "вниз", и мы можем переключится на следующую страницу - переключаемся
         page=page+1
         boxList= []
         renderMenu(titles,page-1,boxList) 
-    elif(Button.UP in buttonsList and page >0): # Если мы нажали вверх, и можем идти вниз - то идём вниз.
+    elif(Button.UP in buttonsList and page >0 and speaker_lock.locked()== False): # Если мы нажали вверх, и можем идти вниз - то идём вниз.
         page=page-1
         boxList= []
         renderMenu(titles,page,boxList)
-    elif(Button.CENTER in buttonsList):# Играем музыку
-        selectedINDEX=selectedINDEX-1 # Я чёрт возьми не помню зачем это нужно
-        #page =0
+    elif(Button.CENTER in buttonsList and speaker_lock.locked()== True):
+        task.close()
+        #speaker_lock.release()
+        #renderMenu(titles,page,boxList)
+    elif(Button.LEFT in buttonsList and speaker_lock.locked()== True):
+        task.close()
+        filesSelectIndex =filesSelectIndex -1
+        if(filesSelectIndex > len(files)-1):
+            filesSelectIndex = 0
+        task = playMusic(selectedINDEX,files,page,filesSelectIndex)
+    elif(Button.RIGHT in buttonsList and speaker_lock.locked()== True):
+        task.close()
+        filesSelectIndex =filesSelectIndex +1
+        if(filesSelectIndex > len(files)-1):
+            filesSelectIndex = 0
+        task = playMusic(selectedINDEX,files,page,filesSelectIndex)    
+    elif(Button.CENTER in buttonsList and speaker_lock.locked()== False):# Играем музыку
+        filesSelectIndex = 0
         if(page >1 and selectedINDEX >=1): # Если и страница и выбранный index больше 1, то
             filesSelectIndex =int((((page*4))+selectedINDEX)-3)# Индекс файла вычисляется так
         elif(page >1 and selectedINDEX <1):# Если страница больше 1, и выбранный index меньше 1 то 
@@ -126,22 +172,13 @@ while (True):# Цикл
             filesSelectIndex=int(page*6-selectedINDEX-1)# Вычисляем вот так
         else: # Если это всё не наш случай, то
             filesSelectIndex=int(selectedINDEX) # Сдаёмся и ставим индекс файла на выбранный вами элемент(если страница - 0, всё будет нормально, если нет - всё не будет так хорошо)
-        # DEBUG INFO / ОТЛАДОЧНАЯ ИНФОРМАЦИЯ
-        #print(page >0)
-        #print(page)
-        #print(filesSelectIndex)
-        #print(files[filesSelectIndex])
-        boxList= []# Список "коробок" в меню(содержит его высоту)
-        wait_first([play(files[selectedINDEX],titles,boxList,page)])# Создаём генератор?
-        tasks = [play(files[filesSelectIndex],titles,boxList,page)] # Создаём задачу
-        for t in tasks: # Исполняем задачи
-            next(t)# Исполняем каждую задачу
-        blocking_wait(10)# Даём системе подумать
-        time.sleep(0.250) # Задержка для кнопок
+        if(filesSelectIndex > len(files)-1):
+            filesSelectIndex = 0
+        task = playMusic(selectedINDEX,files,page,filesSelectIndex)   
    # elif(Button.CENTER in buttonsList and isLocked == True): # Это было нужно для одной вещи
    #     print("Playing!")
    #     time.sleep(0.250)
-    elif(Button.LEFT in buttonsList): # Кнопка выбора вниз
+    elif(Button.LEFT in buttonsList and speaker_lock.locked()== False): # Кнопка выбора вниз
         time.sleep(0.250) # Задержка для кнопок
         if(selectedINDEX > len(boxList)-1): # Если у нас что-то пошло не так(мы на дне)
             selectedINDEX = 0 # Обнуляем всё
